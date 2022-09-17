ae44ce8dd8250f196405d9544838da8aae062dc9
Vladyslav Drushliak <vladislav.drushliak@gmail.com>
2022-09-17 15:36:09 +0300

    Create and configure a project

---
+++ New Year's gift/New Year's gift/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace New_Year_s_gift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
