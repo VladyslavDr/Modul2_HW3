dda8904ddc5fb100f5839715cf5c4c3fceb7e3d2
Vladyslav Drushliak <vladislav.drushliak@gmail.com>
2022-09-19 13:55:07 +0300

    Added a gift and a catalog

--- New Year's gift/New Year's gift/Program.cs
+++ New Year's gift/New Year's gift/Program.cs
@@ -6,6 +6,20 @@ namespace New_Year_s_gift
     {
         public static void Main(string[] args)
         {
+            var catalog = Catalog.GetInstance();
+
+            var myGift = new Gift();
+
+            myGift.ShowGift();
+            myGift.AddItem(catalog.Sweetness[1]);
+            myGift.AddItem(catalog.Sweetness[5]);
+            myGift.AddItem(catalog.Sweetness[0]);
+            myGift.AddItem(catalog.Sweetness[2]);
+            myGift.AddItem(catalog.Sweetness[3]);
+            myGift.ShowGift();
+
+            myGift.Sort();
+            Console.WriteLine($"Total weight: {myGift.TotalWeight().Value}{myGift.TotalWeight().Measure}");
         }
     }
 }
